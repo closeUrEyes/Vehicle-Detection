c7685f854efeb4d50566d7d78fc147aa8ecb7285
closeUrEyes <qichengwen@outlook.com>
2018-05-15 09:18:03 +0800

    train on cpu

--- code/train_eval_image_classifier.py
+++ code/train_eval_image_classifier.py
@@ -16,7 +16,7 @@ def parse_args(check=True):
     parser.add_argument('--checkpoint_exclude_scopes', type=str, default='InceptionV4/Logits,InceptionV4/AuxLogits/Aux_logits')
     parser.add_argument('--train_dir', type=str)
     parser.add_argument('--learning_rate', type=float, default=0.001)
-    parser.add_argument('--clone_on_cpu', type=bool, default=False)
+    parser.add_argument('--clone_on_cpu', type=bool, default=True)
     parser.add_argument('--optimizer', type=str, default='rmsprop')
     parser.add_argument('--batch_size', type=int, default=32)
 
