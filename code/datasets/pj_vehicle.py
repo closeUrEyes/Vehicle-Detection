c491ec204bea301456e5893731fd9cb3b4d858b0
closeUrEyes <qichengwen@outlook.com>
2018-05-17 08:29:21 +0800

    train on gpu

--- code/datasets/pj_vehicle.py
+++ code/datasets/pj_vehicle.py
@@ -71,7 +71,7 @@ def get_split(split_name, dataset_dir, file_pattern=None, reader=None):
 
     keys_to_features = {
         'image/encoded': tf.FixedLenFeature((), tf.string, default_value=''),
-        'image/format': tf.FixedLenFeature((), tf.string, default_value='png'),
+        'image/format': tf.FixedLenFeature((), tf.string, default_value='jpg'),
         'image/class/label': tf.FixedLenFeature(
             [], tf.int64, default_value=tf.zeros([], dtype=tf.int64)),
     }
